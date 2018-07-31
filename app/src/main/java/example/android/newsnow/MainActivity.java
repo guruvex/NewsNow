221957943081baef3f059acf0ad9b089799f43c4
james heath <guruvex@gmail.com>
2018-07-31 10:14:57 -0600

    first commit

---
+++ app/src/main/java/example/android/newsnow/MainActivity.java
@@ -0,0 +1,29 @@
+package example.android.newsnow;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button nextPage = findViewById(R.id.find_news);
+
+        nextPage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ShowToast();
+            }
+        });
+    }
+    public void ShowToast () {
+        Toast.makeText(this, "Find News", Toast.LENGTH_SHORT).show();
+    }
+}
+
