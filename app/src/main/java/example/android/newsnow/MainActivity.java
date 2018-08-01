259cd29361d3aeddb28c230516e8d7d724b5e161
james heath <guruvex@gmail.com>
2018-08-01 10:21:43 -0600

    working on live data from network

--- app/src/main/java/example/android/newsnow/MainActivity.java
+++ app/src/main/java/example/android/newsnow/MainActivity.java
@@ -5,9 +5,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RadioButton;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    public String URLkey;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Button nextPage = findViewById(R.id.find_news);
+
 
         nextPage.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -24,9 +28,30 @@ public class MainActivity extends AppCompatActivity {
         });
     }
     public void ShowToast () {
-        Toast.makeText(this, "Find News", Toast.LENGTH_SHORT).show();
+        // this will build the url based on user selections and pass it to the new intent
+        Toast.makeText(this, R.string.finding_news, Toast.LENGTH_SHORT).show();
 
+        RadioButton rb1 = findViewById(R.id.rb_1);
+        RadioButton rb2 = findViewById(R.id.rb_2);
+        RadioButton rb3 = findViewById(R.id.rb_3);
+        RadioButton rb4 = findViewById(R.id.rb_4);
+        // build url to pass to next activity
+        // url building in simple state to test with
+        if (rb1.isChecked()) {
+            URLkey = "https://content.guardianapis.com/search?q=sports&api-key=a8fc710c-26a0-4c93-85b9-7319adb3f0b9";
+        }
+        if (rb2.isChecked()) {
+            URLkey = "https://content.guardianapis.com/search?q=weather&api-key=a8fc710c-26a0-4c93-85b9-7319adb3f0b9";
+        }
+        if (rb3.isChecked()) {
+            URLkey = "https://content.guardianapis.com/search?q=tech&api-key=a8fc710c-26a0-4c93-85b9-7319adb3f0b9";
+        }
+        if (rb4.isChecked()) {
+            // unassigned temp url used
+            URLkey = "https://content.guardianapis.com/search?q=sports&api-key=a8fc710c-26a0-4c93-85b9-7319adb3f0b9";
+        }
         Intent showNews = new Intent(this, ShowNewsActivity.class);
+        showNews.putExtra("url",URLkey);
         startActivity(showNews);
     }
 }
--- app/src/main/java/example/android/newsnow/ShowNewsActivity.java
+++ app/src/main/java/example/android/newsnow/ShowNewsActivity.java
@@ -4,6 +4,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ListView;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
 
 public class ShowNewsActivity extends AppCompatActivity {
@@ -12,7 +18,11 @@ public class ShowNewsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show_news);
+        // ToDo get url from last activity
 
+        doInBackground();
+
+        // this list will be replaced by url feed
         final ArrayList<NewsItem> newsItem = new ArrayList<>();
         newsItem.add(new NewsItem("test", R.drawable.blackcat));
         newsItem.add(new NewsItem("test", R.drawable.blackcat));
@@ -29,5 +39,53 @@ public class ShowNewsActivity extends AppCompatActivity {
 
         listView.setAdapter(displayNewsAdapter);
 
+    }
+    private URL createUrl(String stringUrl) {
+        // turns the passed string to a url the HttpConnection can handle
+        URL url = null;
+        try {
+            url = new URL(stringUrl);
+        } catch (MalformedURLException exception) {
+            return null;
+        }
+        return url;
+    }
+
+    protected String doInBackground(String... url1) {
+
+        Bundle bundle = getIntent().getExtras();
+        String newURL = bundle.getString("url");
+
+        HttpConnection getNews = new HttpConnection();
+        String jsonString = "";
+        try {
+            jsonString = getNews.makeHttpRequest(createUrl(newURL));
+
+        } catch (IOException e) {
+            return null;
+        }
+
+        /**
+         * if it worked to this point process the
+         * raw jason data is stored in jsonString
+         */
+        StringBuilder data = new StringBuilder(); // string to hold the processed json data
+
+        try {
+            JSONObject rootJsonObj = new JSONObject(jsonString); // build root jason obj
+            JSONArray myJsonArray = rootJsonObj.optJSONArray("results"); // build array from the root object
+
+            for (int i = 0; i < myJsonArray.length(); i++) {
+                JSONObject jsonItems = myJsonArray.getJSONObject(i); // pull out each item in the jason array one at a time.
+
+                String name = jsonItems.optString("artistName").toString();
+                String album = jsonItems.optString("collectionName").toString();
+                String price = jsonItems.optString("collectionPrice").toString();
+                String currency = jsonItems.optString("currency").toString();
+
+            }} catch (Exception e) {
+            System.out.println("error " + e);
+        }
+        return data.toString();
     }
 }
