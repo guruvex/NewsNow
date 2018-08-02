bc2bd4d6b6dda1973cdac6dc21c9b984e43eee72
james heath <guruvex@gmail.com>
2018-08-02 10:34:04 -0600

    work in progress

--- app/src/main/java/example/android/newsnow/NewsItem.java
+++ app/src/main/java/example/android/newsnow/NewsItem.java
@@ -8,14 +8,20 @@ public class NewsItem {
 
     private String mTitle;
     private int mImage;
+    private String mWebURL;
+    private String mSectionName;
 
-    public NewsItem (String title, int image){
+    public NewsItem (String title, int image, String webURL, String sectionName){
         mTitle = title;
         mImage = image;
+        mWebURL = webURL;
+        mSectionName = sectionName;
         }
 
         public String getTitle(){return mTitle;}
         public int getImage(){return mImage;}
+        public String getWebURL(){return mWebURL;}
+        public String getSectionName(){return mSectionName;}
 }
 
 
--- app/src/main/java/example/android/newsnow/ShowNewsActivity.java
+++ app/src/main/java/example/android/newsnow/ShowNewsActivity.java
@@ -1,8 +1,10 @@
 package example.android.newsnow;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
@@ -14,30 +16,31 @@ import java.util.ArrayList;
 
 public class ShowNewsActivity extends AppCompatActivity {
 
+    final ArrayList<NewsItem> newsItem = new ArrayList<>();
+    TextView mTextView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show_news);
-        // ToDo get url from last activity
 
-        doInBackground();
+        mTextView = findViewById(R.id.news_update);
+
+        GetUrlContentTask task = new GetUrlContentTask();
+        task.execute();
 
         // this list will be replaced by url feed
-        final ArrayList<NewsItem> newsItem = new ArrayList<>();
-        newsItem.add(new NewsItem("test", R.drawable.blackcat));
-        newsItem.add(new NewsItem("test", R.drawable.blackcat));
-        newsItem.add(new NewsItem("test", R.drawable.blackcat));
-        newsItem.add(new NewsItem("test", R.drawable.blackcat));
-        newsItem.add(new NewsItem("test", R.drawable.blackcat));
-        newsItem.add(new NewsItem("test", R.drawable.blackcat));
-        newsItem.add(new NewsItem("test", R.drawable.blackcat));
-        newsItem.add(new NewsItem("test", R.drawable.blackcat));
 
-        NewsViewAdaptor displayNewsAdapter = new NewsViewAdaptor (this, newsItem);
+        //newsItem.add(new NewsItem("test", R.drawable.blackcat));
+        //newsItem.add(new NewsItem("test", R.drawable.blackcat));
+        //newsItem.add(new NewsItem("test", R.drawable.blackcat));
+        //newsItem.add(new NewsItem("test", R.drawable.blackcat));
+        //newsItem.add(new NewsItem("test", R.drawable.blackcat));
+        //newsItem.add(new NewsItem("test", R.drawable.blackcat));
+        //newsItem.add(new NewsItem("test", R.drawable.blackcat));
+        //newsItem.add(new NewsItem("test", R.drawable.blackcat));
 
-        ListView listView = (ListView) findViewById(R.id.list);
 
-        listView.setAdapter(displayNewsAdapter);
 
     }
     private URL createUrl(String stringUrl) {
@@ -46,46 +49,71 @@ public class ShowNewsActivity extends AppCompatActivity {
         try {
             url = new URL(stringUrl);
         } catch (MalformedURLException exception) {
+            mTextView.setText("something went wrong");
             return null;
         }
         return url;
     }
 
-    protected String doInBackground(String... url1) {
+    public class GetUrlContentTask extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... strings) {
 
-        Bundle bundle = getIntent().getExtras();
-        String newURL = bundle.getString("url");
+            // get url from last activity
+            Bundle bundle = getIntent().getExtras();
+            String newURL = bundle.getString("url");
 
-        HttpConnection getNews = new HttpConnection();
-        String jsonString = "";
-        try {
-            jsonString = getNews.makeHttpRequest(createUrl(newURL));
+            // testing if i get the url passed
+            mTextView.setText(newURL);
 
-        } catch (IOException e) {
-            return null;
+            HttpConnection getNews = new HttpConnection();
+            String jsonString = "";
+            try {
+                jsonString = getNews.makeHttpRequest(createUrl(newURL));
+
+            } catch (IOException e) {
+                return null;
+            }
+
+            /**
+             * if it worked to this point process the
+             * raw jason data is stored in jsonString
+             */
+            StringBuilder data = new StringBuilder(); // string to hold the processed json data
+
+            try {
+                JSONObject rootJsonObj = new JSONObject(jsonString); // build root jason obj
+                JSONArray myJsonArray = rootJsonObj.optJSONArray("results"); // build array from the root object
+
+                for (int i = 0; i < myJsonArray.length(); i++) {
+                    JSONObject jsonItems = myJsonArray.getJSONObject(i); // pull out each item in the jason array one at a time.
+
+                    String sectionName = jsonItems.optString("sectionName").toString();
+                    String webTitle = jsonItems.optString("webTitle").toString();
+                    String webUrl = jsonItems.optString("webUrl").toString();
+
+                    data.append(sectionName);
+                    data.append(webTitle);
+                    data.append(webUrl);
+
+                    //newsItem.add(new NewsItem(webTitle, R.drawable.blackcat,webUrl,sectionName));
+
+                }} catch (Exception e) {
+                System.out.println("error " + e);
+            }
+            return data.toString();
         }
 
-        /**
-         * if it worked to this point process the
-         * raw jason data is stored in jsonString
-         */
-        StringBuilder data = new StringBuilder(); // string to hold the processed json data
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
 
-        try {
-            JSONObject rootJsonObj = new JSONObject(jsonString); // build root jason obj
-            JSONArray myJsonArray = rootJsonObj.optJSONArray("results"); // build array from the root object
+            TextView mTextView = findViewById(R.id.news_update);
+            mTextView.setText(s);
 
-            for (int i = 0; i < myJsonArray.length(); i++) {
-                JSONObject jsonItems = myJsonArray.getJSONObject(i); // pull out each item in the jason array one at a time.
-
-                String name = jsonItems.optString("artistName").toString();
-                String album = jsonItems.optString("collectionName").toString();
-                String price = jsonItems.optString("collectionPrice").toString();
-                String currency = jsonItems.optString("currency").toString();
-
-            }} catch (Exception e) {
-            System.out.println("error " + e);
+            NewsViewAdaptor displayNewsAdapter = new NewsViewAdaptor (ShowNewsActivity.this, newsItem);
+            ListView listView = (ListView) findViewById(R.id.list);
+            listView.setAdapter(displayNewsAdapter);
         }
-        return data.toString();
     }
 }
